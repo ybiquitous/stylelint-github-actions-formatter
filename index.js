6a624dd49aed412c1688b2677100d08f8f78de4e
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-04-10 01:23:49 +0900

    Add unused var

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const _ = require("lodash");
 
 // @see https://help.github.com/en/actions/reference/workflow-commands-for-github-actions
-module.exports = function githubActionsFormatter(results) {
+module.exports = function githubActionsFormatter(results, a) {
   return _.flatMap(results, (res) => {
     return res.warnings.map((warn) => {
       return `::${warn.severity} file=${res.source},line=${warn.line},col=${warn.column}::${warn.text}`;
